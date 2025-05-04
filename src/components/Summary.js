4ecd7eae006fc118ebe956f4e514407ac764c0c0
Ajay <ajaysengar831@gmail.com>
2025-05-03 21:26:45 -0700

    add code

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -37,7 +37,7 @@ const Summary = ({ formData, prevStep, setStep, setFormData }) => {
             username: '',
             usernameAvailable: null,
             usernameStatus: '',
-            currentPassword: ' ',
+            currentPassword: '',
             newPassword: '',
             profession: '',
             companyName: '',
